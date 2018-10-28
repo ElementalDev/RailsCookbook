271a96d72f7a3f54789f4648673b7b8a6c779f4b
Reece Collett <rcollett@spartaglobal.com>
2018-10-28 09:11:21 +0000

    Create unit tests for Rails install

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,24 @@ describe 'rails::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it "should install rbenv" do
+      expect(chef_run).to install_package("rbenv")
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it "should install ruby" do
+      expect(chef_run).to rbenv_system_install("ruby-2.4.0")
+    end
+
+    it "should install rails" do
+      expect(chef_run).to install_gem_package("rails")
     end
   end
 end
